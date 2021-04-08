3f965b98bfeee8a7f929545f23a8b1a1884099ca
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-04-09 00:18:24 +0300

    feat: add host and port to config for cli argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod api;
 mod backend;
+mod client;
 mod config;
 mod dur;
 mod helpers;
-
 use std::sync::Mutex;
 
 pub use backend::{Backend, Memory};
@@ -15,15 +15,18 @@ pub use dur::Dur;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let data = web::Data::new(Mutex::new(Dur::new(Memory::new(), None)));
+    let config = client::cli();
 
+    let data = web::Data::new(Mutex::new(Dur::new(Memory::new(), Some(config.clone()))));
+
+    eprintln!("dur is running on: {}", &config.host_and_port());
     HttpServer::new(move || {
         App::new()
             .app_data(data.clone())
             .service(api::get_health)
             .service(api::new_request)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(&config.host_and_port())?
     .run()
     .await
 }
